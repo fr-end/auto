b359427f73250316e77b0caab15274394570d504
pinchukd <pinchukd@ua.fm>
2015-08-27 23:11:18 +0300

    fixed testurl for proxy

--- gulp.config.js
+++ gulp.config.js
@@ -14,7 +14,7 @@ module.exports = function(){
             path: '/proxy',
             pathto: 'https://auto.ria.com/blocks_search_ajax/search',
             port: 9000,
-            testurl: '?category_id=1&amp;marka_id=98&amp;model_id=955&amp;state=0#category_id=1&amp;state[0]=0&amp;s_yers[0]=0&amp;po_yers[0]=0&amp;currency=1&amp;marka_id[0]=98&amp;model_id[0]=955&amp;countpage=10&amp;page=1'
+            testurl: '?category_id=1&marka_id=98&model_id=955&state=0&category_id=1&state[0]=0&s_yers[0]=0&po_yers[0]=0&currency=1&marka_id[0]=98&model_id[0]=955&countpage=10&page=1'
         }
     };
     
