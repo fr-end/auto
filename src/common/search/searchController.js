7be38bef9d59ddbca481cb18c04562582f3a00e6
Serhii <belyakov_sergei@ukr.net>
2015-09-01 20:18:52 +0300

    partly refactored searchController functionality to searchView

--- src/common/search/searchController.js
+++ src/common/search/searchController.js
@@ -1,94 +1,68 @@
-module.exports = function(auto,autoSearch){
+module.exports = function(service,view){
 
-	autoSearch.elements['category'].addEventListener('change',function(){
+	view.$selectCategory.addEventListener('change',function(){
 		search.loadMarks();
-	})	
+	})
 
-	autoSearch.elements['mark'].addEventListener('change',function(){
+	view.$selectMark.addEventListener('change',function(){
 		search.loadModels();
 	})
 
-	autoSearch.addEventListener('submit',function(event){
+	view.$searchContainer.addEventListener('submit',function(event){
 		event.preventDefault();
 		search.searchCars();
 	});
 
 	var search = {
 		loadCategories: function(){
-			var self = this;			
-			auto.getCategories()
+			var self = this;
+			service.getCategories()
 				.then(function(data){
-					var categories = JSON.parse(data);
-					var select = document.querySelector('[data-select="category"]');
-					var docFragment = document.createDocumentFragment();
-					categories.forEach(function(item, i) {
-						var option = document.createElement('option');
-						option.setAttribute('value',item.value);
-						option.innerHTML=item.name;
-						docFragment.appendChild(option);
-					});
-					select.appendChild(docFragment);
+					var categoriesArray = JSON.parse(data);
+					view.render('getCategories', categoriesArray);
 					self.loadMarks();
-				});
+			});
 		},		
 		loadMarks: function(){
 			var self = this;
-			var categories=autoSearch.elements['category'];
-			var category = categories.options[categories.selectedIndex].value;	
-			auto.getMarks(category)
-			.then(function(data){
-				var marks = JSON.parse(data);
-				var select = document.querySelector('[data-select="mark"]');
-				var def = select.firstElementChild;
-				var docFragment = document.createDocumentFragment();
-				docFragment.appendChild(def);
-				marks.forEach(function(item, i) {
-					var option = document.createElement('option');
-					option.setAttribute('value',item.value);
-					option.innerHTML=item.name+' ('+item.count+')';
-					docFragment.appendChild(option);
-				});
-				select.innerHTML='';
-				select.appendChild(docFragment);
-				self.loadModels();
+			var categories=view.$selectCategory;
+			var category = categories.options[categories.selectedIndex].value;
+
+			service.getMarks(category)
+				.then(function(data){
+					var marks = JSON.parse(data);
+                    view.render('getMarks', marks);
+
+
+					self.loadModels();
 			});
 		},		
 		loadModels: function(){
-			var categories=autoSearch.elements['category'];
-			var category = categories.options[categories.selectedIndex].value;		
-			var marks=autoSearch.elements['mark'];
-			var mark = marks.options[marks.selectedIndex].value;			
-			auto.getModels(category, mark)
-			.then(function(data){
-				var models = JSON.parse(data);
-				var select = document.querySelector('[data-select="model"]');
-				var def = select.firstElementChild;
-				console.log(def);
-				var docFragment = document.createDocumentFragment();
-				docFragment.appendChild(def);				
-				models.forEach(function(item, i) {
-					var option = document.createElement('option');
-					option.setAttribute('value',item.value);
-					option.innerHTML=item.name+' ('+item.count+')';
-					docFragment.appendChild(option);
-				});
-				select.innerHTML='';
-				select.appendChild(docFragment);
-			});
+            var self = this;
+            var categories=view.$selectCategory;
+            var category = categories.options[categories.selectedIndex].value;
+            var mark = view.$selectMark.options[view.$selectMark.selectedIndex].value;
+
+            service.getModels(category, mark)
+                .then(function(data) {
+                    var models = JSON.parse(data);
+
+                    view.render('getModels', models);
+            });
 		},
 		searchCars: function(){
-			var categories=autoSearch.elements['category'];
+			var categories=view.$selectCategory;
 			var category = categories.options[categories.selectedIndex].value;
-			var marks=autoSearch.elements['mark'];
+			var marks=view.$selectMark;
 			var mark = marks.options[marks.selectedIndex].value;
-			var models=autoSearch.elements['model'];
+			var models=view.$selectModel;
 			var model = models.options[models.selectedIndex].value;			
 			var	searchParams = {
 					categoryId: category,
 				    markaId: mark,
 				    modelId: model
 			};
-			auto.getCarIds(searchParams)
+			service.getCarIds(searchParams)
 				.then(function(data){
 					document.getElementById('cars').innerHTML=JSON.parse(data).result.search_result.ids;
 				});
--- src/common/search/searchView.js
+++ src/common/search/searchView.js
@@ -1,4 +1,98 @@
-var view = document.forms.autoSearch;
 
+
+function View(/*template*/) {
+    //this.template = template;
+
+    this.$searchContainer = document.forms.autoSearch;
+    this.$selectCategory = document.querySelector('[data-select=category]');
+    this.$selectMark = document.querySelector('[data-select=mark]');
+    this.$selectModel = document.querySelector('[data-select=model]');
+
+}
+
+View.prototype.render = function (viewCmd, data) {
+    var self = this;
+    var viewCommands = {
+        getCategories: function () {
+            var select = document.querySelector('[data-select="category"]');
+            var docFragment = document.createDocumentFragment();
+            data.forEach(function (item, i) {
+                var option = document.createElement('option');
+                option.setAttribute('value', item.value);
+                option.innerHTML = item.name;
+                docFragment.appendChild(option);
+            });
+            self.$selectCategory.appendChild(docFragment);
+        },
+        getMarks: function () {
+            var select = document.querySelector('[data-select="mark"]');
+            var def = select.firstElementChild;
+            var docFragment = document.createDocumentFragment();
+            docFragment.appendChild(def);
+            data.forEach(function (item, i) {
+                var option = document.createElement('option');
+                option.setAttribute('value', item.value);
+                option.innerHTML = item.name + ' (' + item.count + ')';
+                docFragment.appendChild(option);
+            });
+            select.innerHTML = '';
+            self.$selectMark.appendChild(docFragment);
+
+        },
+        getModels: function(){
+            var def = self.$selectModel.firstElementChild;
+            console.log(def);
+            var docFragment = document.createDocumentFragment();
+            docFragment.appendChild(def);
+            data.forEach(function(item, i) {
+                var option = document.createElement('option');
+                option.setAttribute('value',item.value);
+                option.innerHTML=item.name+' ('+item.count+')';
+                docFragment.appendChild(option);
+            });
+            self.$selectModel.innerHTML='';
+            self.$selectModel.appendChild(docFragment);
+        }
+    };
+/*
+        // the following code just from todoMVC
+            //self.$todoList.innerHTML = self.template.show(parameter);
+        },
+        removeItem: function () {
+            self._removeItem(parameter);
+        },
+        updateElementCount: function () {
+            self.$todoItemCounter.innerHTML = self.template.itemCounter(parameter);
+        },
+        clearCompletedButton: function () {
+            self._clearCompletedButton(parameter.completed, parameter.visible);
+        },
+        contentBlockVisibility: function () {
+            self.$main.style.display = self.$footer.style.display = parameter.visible ? 'block' : 'none';
+        },
+        toggleAll: function () {
+            self.$toggleAll.checked = parameter.checked;
+        },
+        setFilter: function () {
+            self._setFilter(parameter);
+        },
+        clearNewTodo: function () {
+            self.$newTodo.value = '';
+        },
+        elementComplete: function () {
+            self._elementComplete(parameter.id, parameter.completed);
+        },
+        editItem: function () {
+            self._editItem(parameter.id, parameter.title);
+        },
+        editItemDone: function () {
+            self._editItemDone(parameter.id, parameter.title);
+        }
+    };
+*/
+    viewCommands[viewCmd]();
+};
+
+var view = new View();
 
 module.exports = view;
