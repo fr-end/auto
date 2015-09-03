a956f117510219ddb00daf31028f7261d8900426
pinchukd <pinchukd@ua.fm>
2015-09-03 23:52:36 +0300

    searchResults prerelease

--- src/catalog/searchResults/searchResultsController.js
+++ src/catalog/searchResults/searchResultsController.js
@@ -1,17 +1,28 @@
 
 module.exports = (function () {
 
-	function Controller(view, model) {
+	function Controller(model, view, itemController) {
 		var self = this;
 		self.view = view;
 		self.model = model;	
+		self.itemController = itemController;
+
+		console.dir(view);
+
+		self.view.render('showLoading',{});
 	}
 
 	Controller.prototype = {
 
 		showCars: function(data){
 			var self = this;
-			self.view.render('showCars', data);
+			var cars = data.split(',');
+			self.view.render('showCars', { cars : cars });
+			cars.forEach(function(carId){
+				console.log(carId);
+				var viewPort = document.querySelector('[data-car-id="' + carId + '"]');
+				self.itemController.showCar(carId,viewPort);
+			});
 		}
 	
 	};
--- src/catalog/searchResults/searchResultsModel.js
+++ src/catalog/searchResults/searchResultsModel.js
@@ -1,7 +1,19 @@
 module.exports = (function(){
-	function Model(){
 
+	function Model(service){
+		var self = this;
+		self.cars = [];
+		self.service = service;
 	}
+
+	Model.prototype.getCars = function(data) {
+		var carIds = data.split(',');
+		var self = this;
+		self.cars = [];
+		carIds.forEach(function(item){
+
+		});
+	};
 
 	return Model;
 	
