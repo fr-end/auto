1733e994915893194253991c713ba9ed9ec3e2fd
pinchukd <pinchukd@ua.fm>
2015-09-05 13:46:44 +0300

    line sort again

--- src/catalog/searchResults/searchResults.js
+++ src/catalog/searchResults/searchResults.js
@@ -1,15 +1,15 @@
 module.exports = (function () {
 
     var Model       = require('./searchResultsModel.js');
-    var model       = new Model();
+	var View        = require('./searchResultsView.js');
+	var Controller  = require('./searchResultsController.js');
 
-    var View        = require('./searchResultsView.js');
     var template 	= require('./searchResults.handlebars');
-	var view        = new View(template);
 
+    var model       = new Model();
+	var view        = new View(template);
     var item 		= require('../../common/car-item/item');
 	
-	var Controller  = require('./searchResultsController.js');
     var controller  = new Controller(model, view, item);
 
     return controller;
--- src/common/car-item/item.js
+++ src/common/car-item/item.js
@@ -1,12 +1,13 @@
 module.exports = (function () {
 
-	var service 	= require('../../library/auto/autoService');
-
 	var View 		= require('./itemView');
+	var Controller 	= require('./itemController');	
+
 	var template	= require('./car-item.handlebars');
+
+	var service 	= require('../../library/auto/autoService');
 	var view 		= new View(template);	
 
-    var Controller 	= require('./itemController');	
 	var controller 	= new Controller(service, view);
 
     return controller;
