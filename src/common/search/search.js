1f350c7aacfd500868fb142717a4e5fa3cacaf65
pinchukd <pinchukd@ua.fm>
2015-09-05 12:43:06 +0300

    all templates now varialbles

--- src/common/search/search.js
+++ src/common/search/search.js
@@ -2,13 +2,19 @@ module.exports = (function(){
 
 	var service = require('../../library/auto/autoService.js');
 
-	var View 		= require('./searchView.js');
-	var Controller 	= require('./searchController.js');
+	var View 			= require('./searchView.js');
+	var Controller 		= require('./searchController.js');
 
-	var view 		= new View()
-	var searchResults = require('../../catalog/searchResults/searchResults.js');
+	var templates		= {
+		main: 				require('./templates/searchPanel.handlebars'),
+		options: 			require('./templates/options.handlebars'),
+		optionsWithCount: 	require('./templates/optionsWithCount.handlebars')
+	};
+	
+	var view 			= new View(templates);
+	var searchResults 	= require('../../catalog/searchResults/searchResults.js');
 
-	var controller 	= new Controller(service, view, searchResults);
+	var controller 		= new Controller(service, view, searchResults);
 
 	return controller;
 
