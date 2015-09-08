7a833ae693d9c40cba6dc216f2296a14e814d8c1
pinchukd <pinchukd@ua.fm>
2015-09-08 12:54:17 +0300

    autoService new style data return in function getCar2

--- src/library/auto/autoService.js
+++ src/library/auto/autoService.js
@@ -83,7 +83,46 @@ module.exports = (function(){
 						'/?lang_id=' + langId;
 			ajax.get( url, deferred );
 			return deferred.promise;
+		},		
+		getCar2: function ( carId ) {
+			//https://auto.ria.com/blocks_search_ajax/view/auto/14356030/?lang_id=2
+			var deferred=Q.defer();
+			var langId = 2;
+			var url = config.autoRiaUaHost + '/blocks_search_ajax/view/auto/'+
+						carId +
+						'/?lang_id=' + langId;
+			ajax.get( url, deferred );
+			return deferred.promise.then(function (data) {
+						var carInfo = JSON.parse(data);
+						var autoData = carInfo.result.auto_data;
+						var imgUrl = carInfo.result.photo_data.photo ? carInfo.result.photo_data.photo.url : "";
+						imgUrl = imgUrl.replace(".","f.");
+						var carInfoNeeded = {
+							carId 		: autoData.auto_id,
+							title 		: autoData.marka_data.name + " " + autoData.model_data.name + " " 
+											+ autoData.version,							
+							categoryId 	: carInfo.result.category_data.category_id,
+							markaId 	: autoData.marka_id,
+							modelId 	: autoData.model_id,
+							version		: autoData.version,
+							race 		: autoData.race.race,
+							fuel 		: autoData.fuel_data ? autoData.fuel_data.name : "---",
+							volume 		: autoData.engineVolume || "---",
+							gearBox 	: autoData.gearbox_data ? autoData.gearbox_data.name : "---",
+							price 		: {
+											usd : carInfo.result.price_data.prices[1],
+											uah : Math.round(carInfo.result.price_data.prices[3])
+										},
+							img 		: imgUrl ? "https://cdn.riastatic.com/photos/" + imgUrl : "https://img.auto.ria.com/images/no-photo/no-photo-380x250.jpg",
+							date 		: carInfo.result.date_data.date_add.day+"."+carInfo.result.date_data.date_add.full_month+"."+carInfo.result.date_data.date_add.year,
+							description : autoData.description,
+							phone 		: carInfo.result.user_phones[0].phone_formatted,
+							city 		: carInfo.result.location_data.state.region_name,
+						};						
+						return carInfoNeeded;
+					});
 		}
+
 
 	};
 
