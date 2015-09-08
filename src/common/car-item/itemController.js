302223aa322c9dc08dc45a006a6f8ac6fa7cd50a
blyzniuk <blyzniuk.oleksandr@gmail.com>
2015-09-08 13:59:15 +0300

    Swich getCar to getCar2

--- src/common/car-item/itemController.js
+++ src/common/car-item/itemController.js
@@ -12,31 +12,13 @@ module.exports = (function(){
 		showCar: function(carId){
 			console.log(carId);
 			var self = this;
-			self.service.getCar(carId)
+			self.service.getCar2(carId)
 				.then(function(data){
-					var carInfo = JSON.parse(data);
-					var imgUrl = carInfo.result.photo_data.photo ? carInfo.result.photo_data.photo.url : "";
-					imgUrl = imgUrl.replace(".","f.");
-					var carInfoNeeded = {
-						img : imgUrl ? "https://cdn.riastatic.com/photos/" + imgUrl : "https://img.auto.ria.com/images/no-photo/no-photo-380x250.jpg",
-						carId : carInfo.result.auto_data.auto_id,
-						title : carInfo.result.auto_data.marka_data.name + " " + carInfo.result.auto_data.model_data.name + " " 
-								+ carInfo.result.auto_data.version,
-						priceUSD : carInfo.result.price_data.prices[1],
-						priceUAH : Math.round(carInfo.result.price_data.prices[3]),
-						date : carInfo.result.date_data.date_add.day+"."+carInfo.result.date_data.date_add.full_month+"."+carInfo.result.date_data.date_add.year,
-						description : carInfo.result.auto_data.description,
-						race : carInfo.result.auto_data.race.race,
-						fuel : carInfo.result.auto_data.fuel_data ? carInfo.result.auto_data.fuel_data.name : "---",
-						volume : carInfo.result.auto_data.engineVolume || "---",
-						transmission : carInfo.result.auto_data.gearbox_data ? carInfo.result.auto_data.gearbox_data.name : "---",
-						tel : carInfo.result.user_phones[0].phone_formatted,
-						city : carInfo.result.location_data.state.region_name,
-					};
-					self.view.render(carInfoNeeded);
+					data.inList = !!localStorage.getItem( carId );
+					self.view.render(data);
 			});
 		}
-	}
+	};
 
 	return Controller;
 
--- src/library/auto/autoService.js
+++ src/library/auto/autoService.js
@@ -95,26 +95,29 @@ module.exports = (function(){
 			return deferred.promise.then(function (data) {
 						var carInfo = JSON.parse(data);
 						var autoData = carInfo.result.auto_data;
-						var imgUrl = carInfo.result.photo_data.photo ? carInfo.result.photo_data.photo.url : "";
-						imgUrl = imgUrl.replace(".","f.");
+						var imgUrl = carInfo.result.photo_data.photo ? carInfo.result.photo_data.photo.url : '';
+						imgUrl = imgUrl.replace('.','f.');
 						var carInfoNeeded = {
 							carId 		: autoData.auto_id,
-							title 		: autoData.marka_data.name + " " + autoData.model_data.name + " " 
-											+ autoData.version,							
+							title 		: autoData.marka_data.name + ' ' + autoData.model_data.name +
+								 ' ' + autoData.version,							
 							categoryId 	: carInfo.result.category_data.category_id,
 							markaId 	: autoData.marka_id,
 							modelId 	: autoData.model_id,
 							version		: autoData.version,
 							race 		: autoData.race.race,
-							fuel 		: autoData.fuel_data ? autoData.fuel_data.name : "---",
-							volume 		: autoData.engineVolume || "---",
-							gearBox 	: autoData.gearbox_data ? autoData.gearbox_data.name : "---",
+							fuel 		: autoData.fuel_data ? autoData.fuel_data.name : '---',
+							volume 		: autoData.engineVolume || '---',
+							gearBox 	: autoData.gearbox_data ? autoData.gearbox_data.name : '---',
 							price 		: {
 											usd : carInfo.result.price_data.prices[1],
 											uah : Math.round(carInfo.result.price_data.prices[3])
 										},
-							img 		: imgUrl ? "https://cdn.riastatic.com/photos/" + imgUrl : "https://img.auto.ria.com/images/no-photo/no-photo-380x250.jpg",
-							date 		: carInfo.result.date_data.date_add.day+"."+carInfo.result.date_data.date_add.full_month+"."+carInfo.result.date_data.date_add.year,
+							img 		: imgUrl ? 'https://cdn.riastatic.com/photos/' + 
+								imgUrl : 'https://img.auto.ria.com/images/no-photo/no-photo-380x250.jpg',
+							date 		: carInfo.result.date_data.date_add.day + '.' + 
+								carInfo.result.date_data.date_add.full_month + '.' + 
+								carInfo.result.date_data.date_add.year,
 							description : autoData.description,
 							phone 		: carInfo.result.user_phones[0].phone_formatted,
 							city 		: carInfo.result.location_data.state.region_name,
