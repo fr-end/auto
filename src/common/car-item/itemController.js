ba261d410c4b565fdcb060ed2624549a10937220
pinchukd <pinchukd@ua.fm>
2015-09-14 23:46:14 +0300

    itemController self to bind refactor

--- src/common/car-item/itemController.js
+++ src/common/car-item/itemController.js
@@ -10,18 +10,16 @@ module.exports = (function(){
         this.service = service;
         this.view = new View(template);
 
-        self = this;
+		this.events.subscribe('search ids', (function(ids){
+			ids.forEach((function (carId) {
+				this.showCar(carId);
+			}).bind(this));
+		}).bind(this));
 
-		self.events.subscribe('search ids', function(ids){
-			ids.forEach(function (carId) {
-				self.showCar(carId);
-			});
-		});
-
-		self.view.bind('clickAddToWishListButton',function(carID){
-			var result = self.toggleWishList(carID);
-			self.view.toggleClass(carID, result);
-		});
+		this.view.bind('clickAddToWishListButton',(function(carID){
+			var result = this.toggleWishList(carID);
+			this.view.toggleClass(carID, result);
+		}).bind(this));
 
 
 
@@ -30,12 +28,11 @@ module.exports = (function(){
 
     Controller.prototype = {
 		showCar: function(carId){
-			var self = this;
-			self.service.getCar2(carId)
-				.then(function(data){
-					data.inList = self.inList( carId );
-					self.view.render(data);
-			});
+			this.service.getCar2(carId)
+				.then((function(data){
+					data.inList = this.inList( carId );
+					this.view.render(data);
+			}).bind(this));
 		},
 		inList: function(carId){
 			return localService.inList(carId);
