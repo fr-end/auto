068efbe166677ff5c3f9c206edc836bc97a94f8d
blyzniuk <blyzniuk.oleksandr@gmail.com>
2015-10-02 12:16:55 +0300

    Big img added

--- src/modules/carPage/CarPageView.js
+++ src/modules/carPage/CarPageView.js
@@ -4,10 +4,13 @@ module.exports = (function () {
         this.$viewPort = document.querySelector('.main');
         this.inListSelector = '.carpage-star';
         this.viewPortSelector = '.carpage-wraper';
+        this.sliderLittleImgSelector = '.carpage-slider-carousel-cars-item-link__img';
     }
 
     View.prototype.render = function (data) {
         this.$viewPort.innerHTML = data;
+        this.$sliderBigImg = document.querySelector('.carpage-slider-bigimg__img');
+        this.$sliderBigImg.src = document.querySelector(this.sliderLittleImgSelector).src;
     };
 
     View.prototype.bind = function (event, handler) {
@@ -29,6 +32,12 @@ module.exports = (function () {
         this.$viewPort = this.$viewPort || document.querySelector(this.viewPortSelector);
         this.$viewPort.classList.toggle('in-list', result);
     };
+    
+    View.prototype.toggleSliderBigImg = function ($nextImg) {
+        document.querySelector(this.sliderLittleImgSelector + ' .active').classList.toggle('active');
+        $nextImg.classList.toggle('active');
+        this.$sliderBigImg.src = $nextImg.src;
+    }
 
     return View;
 
