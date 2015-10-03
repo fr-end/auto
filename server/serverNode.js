42987dfa258033809a3e463c0ca08272ff418764
Serhii <belyakov_sergei@ukr.net>
2015-10-03 15:02:41 +0300

    connect to dima"s mongo server instead of localhost

--- server/serverNode.js
+++ server/serverNode.js
@@ -1,5 +1,5 @@
 
-//var util = require('util');
+var util = require('util');
 var mongoose = require('mongoose');
 var express = require('express');
 
@@ -7,31 +7,45 @@ var middleware = require('./middleware/index.js');
 var routes = require('./routes');
 
 
-
-
-
-
-
-
-
 var port = 8888;
 
+var password = require('../../pass.js');
+console.log(password);
+// for connection to the mongo hosted by Dima's server
+var auth = {
+    user: 'admin',
+    password: password,
+    host: '176.111.61.10',
+    name: 'auto'
+};
+var urlMongo = util.format('mongodb://%s:%s@%s/%s', auth.user, auth.password, auth.host, auth.name);
+
+// to connect via the mongo shell use the following command
+// mongo --host 176.111.61.10 -u admin -p fr-end --authenticationDatabase auto
+//console.log(urlMongo);
+// 'mongodb://admin:<some_pass_goes_here>@176.111.61.10/auto'
+
+
+// via mongolab
 //var auth = {
 //    user: 'fr-end',
-//    pass: '<yourPassGoesHere>',
+//    password: '<yourPassGoesHere>',
 //    host: 'ds041693.mongolab.com',
 //    port: 41693,
 //    name: 'auto'
 //};
+// var urlMongo = util.format('mongodb://%s:%s@%s:%d/%s', auth.user, auth.password, auth.host, auth.port, auth.name);
+//console.log(urlMongo);
+//'mongodb://fr-end:<yourPassGoesHere>@ds041693.mongolab.com:41693/auto'
 
-//var urlMongo = util.format('mongodb://%s:%s@%s:%d/%s', auth.user, auth.pass, auth.host, auth.port, auth.name);
-var urlMongo = 'mongodb://localhost:27017/auto';
-console.log(urlMongo);
-
-// to connect via shell use the following command
+// to connect via the mongo shell use the following command
 // mongo ds041693.mongolab.com:41693/auto -u fr-end -p '<yourPassGoesHere>'
 
-//'mongodb://fr-end:<yourPassGoesHere>@ds041693.mongolab.com:41693/auto'
+
+// via localhost
+//var urlMongo = 'mongodb://localhost:27017/auto';
+//console.log(urlMongo);
+
 
 function noSuchUserWithEmail(email){
     console.log('there is no such user with the', email, 'email');
@@ -49,7 +63,7 @@ mongoose.connect(urlMongo, function (err) {
 
     if(err) throw err;
 
-    //console.log('connected');
+    console.log('connected to mongo');
 
     var app = express();
 
