7305768042a7b6734b30d7ddb02a19730b1cd014
igor0ser <igor0ser@gmail.com>
2015-10-12 18:36:22 +0300

    errors fixed

--- server/helpers/invalidData.js
+++ server/helpers/invalidData.js
@@ -2,13 +2,13 @@ module.exports = function invalidData(type, form, email){
     var errors = [];
     var text;
     if (type === 'no user'){
-        text = 'Нет пользователя с таким ' + email + ' e-mail';
+        text = 'Нет пользователя с таким e-mail';
         errors.push(new CustomError(type, text, form))
     } else if (type === 'user already exists') {
-        text = 'Пользователь ' + email + ' уже существует. Пожалуйста, войдите в аккаунт.';
+        text = 'Пользователь уже существует.';
         errors.push(new CustomError(type, text, form))
     } else if (type === 'wrong password') {
-        text = 'Неверный пароль для пользователя ' +  email;
+        text = 'Неверный пароль';
         errors.push(new CustomError(type, text, form))
     }
     console.log('errors in invalidData func ',errors);
